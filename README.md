1bb985f62577cc2b4238b7c8832a5a591ed1c3f8
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2024-04-23 22:42:42 +0900

    Implemented the process of denoising

---
+++ README.md
@@ -0,0 +1,10 @@
+# DDPM implementation
+
+![DDPM image](./resources/DDPM_image.webp)
+
+The implementation of a simple DDPM.
+
+## References
+
+* [Generating images with DDPMs: A PyTorch Implementation](https://medium.com/@brianpulfer/enerating-images-with-ddpms-a-pytorch-implementation-cef5a2ba8cb1)
+* [Diffusion Modelの解説論文](https://qiita.com/suikabar/items/f23f83df0febdd50125c)
---
+++ main.py
@@ -0,0 +1,156 @@
+import random
+import imageio
+import numpy as np
+from argparse import ArgumentParser
+
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+
+import einops
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+from torchvision.transforms import Compose, ToTensor, Lambda
+from torchvision.datasets.mnist import MNIST, FashionMNIST
+
+SEED = 0
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+
+STORE_PATH_MNIST = f"ddpm_model_mnist.pt"
+STORE_PATH_FASHION = f"ddpm_model_fashion.pt"
+
+no_train = False
+fashion = True
+batch_size = 1
+n_epochs = 20
+lr = 0.001
+store_path = "ddpm_fashion.pt" if fashion else "ddpm_mnist.pt"
+
+def show_images(images, title=""):
+    if type(images) is torch.Tensor:
+        images = images.detach().cpu().numpy()
+
+    fig = plt.figure(figsize=(8, 8))
+    rows = int(len(images) ** 0.5)
+    cols = round(len(images) / rows)
+
+
+
+    idx = 0
+    for r in range(rows):
+        for c in range(cols):
+            fig.add_subplot(rows, cols, idx + 1)
+
+            if idx < len(images):
+                plt.imshow(images[idx][0], cmap="gray")
+                idx += 1
+    fig.suptitle(title, fontsize=30)
+
+    plt.show()
+
+def show_first_batch(loader):
+    for batch in loader:
+        show_images(batch[0], "First batch")
+        break
+
+def show_forward(ddpm, loader, device):
+    for batch in loader:
+        imgs = batch[0]
+
+        show_images(imgs, "Original")
+
+        for percent in [0.25, 0.5, 0.75, 1]:
+            show_images(
+                ddpm(imgs.to(device),[int(percent * ddpm.n_steps) - 1 for _ in range(len(imgs))]), f"DDPM Noisy images {int(percent * 100)}%")
+            break
+
+def generate_new_images(ddpm, n_samples=16, device=None, frames_per_gif=100, gif_name="sampling.gif",c=1, h=28, w=28):
+    frame_idxs = np.linespace(0,ddpm.n_steps, frames_per_gif).astype(np.uint)
+    frames = []
+
+    with torch.no_grad():
+        if device is None:
+            device = ddpm.device
+
+        x = torch.randn(n_samples, c, h ,w).to(device)
+
+        for idx, t in enumerate(list(range(ddpm.n_steps))[::-1]):
+            time_tensor = (torch.ones(n_samples, 1) * t).to(device).long()
+            eta_theta = ddpm.backward(x,time_tensor)
+
+            alpha_t = ddpm.alphas[t]
+            alpha_t_bar = ddpm.alpha_bars[t]
+
+            x = (1 / alpha_t.sqrt()) * (x - ( 1 - alpha_t) / (1 - alpha_t_bar).sqrt() * eta_theta)
+
+            if t > 0:
+                z = torch.randn(n_samples, c, h, w).to(device)
+
+                beta_t = ddpm.betas[t]
+                sigma_t = beta_t.sqrt()
+
+                prev_alpha_t_bar = ddpm.alpha_bars[t-1] if t > 0 else ddpm.alphas[0]
+                beta_tilda_t = ((1 - prev_alpha_t_bar) / (1 - alpha_t_bar)) * beta_t
+                sigma_t = beta_tilda_t.sqrt()
+
+                x = x + sigma_t * z
+
+            if idx in frame_idxs or t == 0:
+                normalized = x.clone()
+                for i in range(len(normalized)):
+                    normalized[i] -= torch.min(normalized[i])
+                    normalized[i] *= 255 / torch.max(normalized[i])
+
+                frame = einops.rearrange(normalized, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=int(n_samples ** 0.5))
+                frame = frame.cpu().numpy().astype(np.uint8)
+
+                frames.append(frame)
+
+        with imageio.get_write(gif_name, mode="I") as writer:
+            for idx, frame in enumerate(frames):
+                writer.append_data(frame)
+                if idx == len(frames) - 1:
+                    for _ in range(frames_per_gif // 3):
+                        writer.append_data(frames[-1])
+
+        return x
+
+
+transform = Compose([
+    ToTensor(),
+    Lambda(lambda x : (x-0.5) * 2)
+])
+ds_fn = FashionMNIST if fashion else MNIST
+dataset = ds_fn("./datasets",download=True, train=True, transform=transform)
+loader = DataLoader(dataset,batch_size, shuffle=True)
+
+
+show_first_batch(loader)
+
+class MyDDPM(nn.Module):
+    def __init__(self, network,n_steps=200, min_beta=10**(-4), max_beta = 0.02, device=None, image_chw=(1,28,28)):
+        super(MyDDPM, self).__init__()
+        self.n_steps = n_steps
+        self.device = device
+        self.image_chw = image_chw
+        self.network = network.to(device)
+        self.betas = torch.linspace(min_beta, max_beta, n_steps).to(device)
+        self.alphas = 1 - self.betas
+        self.alpha_bars = torch.tensor([torch.prod(self.alphas[:i+1]) for i in range(len(self.alphas))]).to(device)
+
+    def forward(self, x0, t, eta=None):
+        n, c, h, w = x0.shape
+        a_bar = self.alpha_bars[t]
+
+        if eta is None:
+            eta = torch.randn(n, c, h, w).to(self.device)
+
+        noisy = a_bar.sqrt().reshape(n,1,1,1) * x0 + (1 - a_bar).sqrt().reshape(n,1,1,1) * eta
+        return noisy
+
+    def backward(self, x, t):
+        self.network(x,t)
