246f4ab965a5dc7d34d6037f650e2c23da4c12b7
kazuki <kitano-kazuki954@e.c..u-tokyo.ac.jp>
2024-04-24 01:40:23 +0900

    Implementation of embedding

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 ![DDPM image](./resources/DDPM_image.webp)
 
-The implementation of a simple DDPM.
+![DDPM algorithm](./resources/DDPM_algorithm.webp)
 
 ## References
 
 * [Generating images with DDPMs: A PyTorch Implementation](https://medium.com/@brianpulfer/enerating-images-with-ddpms-a-pytorch-implementation-cef5a2ba8cb1)
 * [Diffusion Modelの解説論文](https://qiita.com/suikabar/items/f23f83df0febdd50125c)
+* [PyTorchでCNNを徹底解説](https://qiita.com/mathlive/items/8e1f9a8467fff8dfd03c)
--- main.py
+++ main.py
@@ -119,6 +119,22 @@ def generate_new_images(ddpm, n_samples=16, device=None, frames_per_gif=100, gif
 
         return x
 
+def sinusoidal_embedding(n,d):
+    embedding = torch.zeros(n, d)
+    wk = torch.tensor([1 / 10_000 ** (2 * j / d) for j in range(d)])
+    wk = wk.reshape((1,d))
+    t = torch.arange(n).reshape((n, 1))
+    embedding[:,::2] = torch.sin(t * wk[:,::2])
+    embedding[:,1::2] = torch.cos(t * wk[:,::2])    # isn't wk[:,1::2] correct?
+    return embedding
+
+def _make_te(self, dim_in, dim_out):
+    return nn.Sequential(
+        nn.Linear(dim_in, dim_out),
+        nn.SiLU(),
+        nn.Linear(dim_out, dim_out)
+    )
+
 
 transform = Compose([
     ToTensor(),
@@ -154,3 +170,20 @@ class MyDDPM(nn.Module):
 
     def backward(self, x, t):
         self.network(x,t)
+
+class MyBlock(nn.Module):
+    def __init__ (self, shape, in_c, out_c, kernel_size=3, stride=1, padding=1, activation=None, normalize=True):
+        super(MyBlock, self).__init__()
+        self.ln = nn.LayerNorm(shape)
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size, stride,padding )
+        self.conv2 = nn.Conv2d(in_c, out_c, kernel_size, stride,padding )
+        self.activation = nn.SiLU() if activation is None else activation
+        self.normalize = normalize
+
+    def forward(self, x):
+        out = self.ln(x) if self.normalize else x
+        out = self.conv1(out)
+        out = self.activation(out)
+        out = self.conv2(out)
+        out = self.activation(out)
+        return out
